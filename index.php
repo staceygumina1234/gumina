569e37189886ad9d87f282e73c5bfe45ee50e4e5
staceygumina1234 <manilyngumina12@gmail.com>
2024-11-11 19:08:46 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="wid5th=device-width, initial-scale=1">
+	<title>ubuntu server-php deployment</title>
+</head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<body>
+
+<p>ubuntu server-php deployment- Manilyn Gumina</p>
+</body>
+
+<div>
+	<label>Firstname:</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+<div class="container">
+    <label>Middle:</label>
+	<input type="text" name="middle" class="form-control">
+</div>
+<div class="container">
+    <label>Lastname:</label>
+	<input type="text" name="lastname" class="form-control">
+</div>
+<div class="container">
+    <label>Age:</label>
+	<input type="text" name="age" class="form-control">
+</div>
+<div class="container">
+    <label>Address:</label>
+	<input type="text" name="address" class="form-control">
+</div>
+<div class="container">
+    <label>Course&Section:</label>
+	<input type="text" name="course&section" class="form-control">
+</div>
+<div class="container">
+    <label>Submit:</label>
+	<input type="text" name="submit" class="form-control">
+</div>
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
